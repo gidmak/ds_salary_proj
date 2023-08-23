921896f78d40222161808d526743e2dd62c0edd1
gidmak <gideonmarkus@gmail.com>
2023-08-23 13:32:16 +0100

    finished model building

--- model_building.py
+++ model_building.py
@@ -57,5 +57,47 @@ lm.fit(X_train, y_train)
 np.mean(cross_val_score(lm, X_train, y_train, scoring = 'neg_mean_absolute_error', cv=3))
 
 # Lasso regression
-lm_l = Lasso()
+lm_l = Lasso(alpha = .16)
+lm_l.fit(X_train, y_train)
 np.mean(cross_val_score(lm_l, X_train, y_train, scoring = 'neg_mean_absolute_error', cv=3))
+
+alpha = []
+error = []
+
+for i in range(1, 100):
+    alpha.append(i/100)
+    lml = Lasso(alpha=(i/100))
+    error.append(np.mean(cross_val_score(lml, X_train, y_train, scoring='neg_mean_absolute_error', cv=3)))
+   
+plt.plot(alpha, error)
+err = tuple(zip(alpha, error))
+df_err = pd.DataFrame(err, columns = ['alpha','error'])
+df_err[df_err.error == max(df_err.error)]
+
+# Random forest
+from sklearn.ensemble import RandomForestRegressor
+rf = RandomForestRegressor()
+
+np.mean(cross_val_score(rf, X_train, y_train, scoring='neg_mean_absolute_error', cv=3))
+
+# Tune models GridsearchCV
+from sklearn.model_selection import GridSearchCV
+parameters = {'n_estimators':range(10,100,10), 'criterion':('squared_error','absolute_error'), 'max_features':('auto', 'sqrt','log2')}
+
+gs = GridSearchCV(rf, parameters, scoring='neg_mean_absolute_error', cv=3)
+gs.fit(X_train, y_train)
+
+gs.best_score_
+gs.best_estimator_
+
+# Test ensembles 
+tpred_lm = lm.predict(X_test)
+tpred_lml = lm_l.predict(X_test)
+tpred_rf = gs.best_estimator_.predict(X_test)
+
+from sklearn.metrics import mean_absolute_error
+mean_absolute_error(y_test, tpred_lm)
+mean_absolute_error(y_test, tpred_lml)
+mean_absolute_error(y_test, tpred_rf)
+
+mean_absolute_error(y_test,(tpred_lm+tpred_rf)/2)
